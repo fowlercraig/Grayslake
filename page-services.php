adfb00e2ba3085e7f164cf86ad30693e4de67c40
Craig Fowler <craig@wearegiants.com>
2016-03-22 11:33:10 -0700

    updated services

--- page-services.php
+++ page-services.php
@@ -9,59 +9,39 @@ $thumb_url = $thumb_url_array[0];
 
 <article <?php post_class(); ?> style="background-image:url(<?php echo $thumb_url ; ?>);">
 <div class="fs-row">
-<div class="fs-cell fs-lg-8 fs-md-5 fs-sm-3 fs-centered">
+<div class="fs-cell fs-lg-11 fs-md-5 fs-sm-3 fs-centered">
+
 <header class="hentry__header">
 <h2 class="hentry__title text-center color__white"><span><?php the_title(); ?></span></h2>
 </header>
-<div class="hentry__content">
-<?php $services = get_field('services'); ?>
-<div class="fs-row">
-<?php foreach ($services as $service): ?>
 
-<div class="fs-cell fs-lg-half fs-md-half fs-sm-3">
-<h4 class="text-center color__white"><?php echo $service['title']; ?></h4>
+<div class="hentry__content hentry__content--md">
+
+<div class="fs-row">
+
+<?php $services = get_field('services'); ?>
+<?php $i = 0; foreach ($services as $service): ?>
+
+<?php if($i == 0): ?><div class="fs-cell fs-lg-half fs-md-full fs-sm-3"><?php endif; ?>
+<?php if($i == 2): ?><div class="fs-cell fs-lg-half fs-md-full fs-sm-3"><?php endif; ?>
+
+<div class="text-left">
+<hr class="divider bg__color-white--50">
+<h4 class="text-left color__white"><?php echo $service['title']; ?></h4>
 <?php echo $service['description']; ?>
 </div>
 
-<?php endforeach; ?>
+<?php if($i == 1): ?></div><?php endif; ?>
+<?php if($i == 4): ?></div><?php endif; ?>
+
+<?php $i++; endforeach; ?>
+</div>
+
+</div>
 </div>
 </div>
 </div>
 </div>
 </article>
-
-<?php get_footer(); ?>
-
-<?php Themewrangler::setup_page();get_header(/***Template Name: About */); ?>
-
-<div <?php post_class('services bg__color-bgGray');?>>
-	<div class="page__content">
-		<div class="fs-row">
-			<div class="page__header text-center">
-				<h1 class="page__title"><?php the_title(); ?></h1>
-			</div>
-		</div><!-- fs-row -->
-		<div class="fs-row">
-			<div class="fs-cell fs-lg-half fs-md-full fs-sm-full">
-
-<?php $services = get_field('services'); ?>
-<?php foreach ($services as $service): ?>
-
-					<a data-tabs-group="tab-1" class="btn btn__tab tab accordion__title" href="#<?php echo seoUrl($service['title']); ?>"><?php echo $service['title']; ?></a></li>
-
-<?php endforeach; ?>
-
-			</div>
-			<div class="fs-cell fs-lg-half fs-md-full fs-sm-full">		
-
-<?php foreach ($services as $service): ?>
-
-				<div id="<?php echo seoUrl($service['title']); ?>"><?php echo $service['description']; ?></div>
-
-<?php endforeach; ?>
-			</div>
-		</div><!-- fs-row -->
-	</div><!-- page-content -->
-</div><!-- services -->
 
 <?php get_footer(); ?>
