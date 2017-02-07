ab0eaccfd493da1adb3a376ee384c901776b4198
Craig Fowler <craig@wearegiants.com>
2017-02-06 21:10:03 -0800

    removed stuff

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ add_theme_support('soil-clean-up');
 add_theme_support('soil-google-analytics', 'UA-35986901-23');
 add_theme_support('soil-disable-trackbacks');
 add_theme_support('soil-disable-asset-versioning');
-add_theme_support('soil-nav-walker');
+//add_theme_support('soil-nav-walker');
 
 // Remove Admin Bar
 
